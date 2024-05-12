c7a8f5ceab6239c7a9f6ba58c1ae510345738ac2
Bobbaru <bobbaru_krueger@yahoo.com>
2024-05-12 03:15:44 +0300

    home link in header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <div className="container py-4">
+            <Link href="/">Home</Link>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
