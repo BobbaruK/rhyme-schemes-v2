731999ab7695d61ca58e65798387a6cde6cee557
Bobbaru <bobbaru_krueger@yahoo.com>
2024-05-16 21:21:49 +0300

    MainDeck and AlbumSongHero components update

--- src/components/deck/MainDeck.tsx
+++ src/components/deck/MainDeck.tsx
@@ -1,20 +1,106 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import { useDeckStore } from "@/stores/deck-store-provider";
+import Image from "next/image";
+import { PiPlaylistDuotone } from "react-icons/pi";
+import noImage from "../../../public/noimage.svg";
+import { Card, CardDescription, CardHeader } from "../ui/card";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const MainDeck = () => {
-  const [count, setCount] = useState<number>(0);
+  const router = useRouter();
 
-  useEffect(() => {
-    setInterval(() => {
-      // setCount(Math.floor(Math.random() * 10));
-      setCount((count) => count + 1);
-    }, 1000);
-  }, []);
+  const { songDetails, playlist, loadSongDetails } = useDeckStore(
+    (state) => state,
+  );
+
+  if (!songDetails) return null;
+
+  console.log(playlist && playlist[0].album?.artwork.url);
 
   return (
     <div className="sticky bottom-0 left-0 right-0 bg-primary py-4 text-primary-foreground">
-      <div className="container">MainDeck: {count}</div>
+      <div className="container flex items-center justify-between">
+        <div>
+          <p>id: {songDetails?.id}</p>
+          <p>song: {songDetails?.songName}</p>
+          <p>artist: {songDetails?.songArtist}</p>
+        </div>
+        <Sheet>
+          <SheetTrigger>
+            <PiPlaylistDuotone size={30} />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col gap-4 overflow-y-auto p-2 py-3">
+            <SheetHeader>
+              <SheetTitle>Playlist</SheetTitle>
+            </SheetHeader>
+            <div className="flex flex-col gap-1">
+              {playlist?.map((song) => (
+                <Card
+                  key={song.id}
+                  className={cn(
+                    `flex items-center justify-start gap-4 overflow-hidden ${song.id === songDetails.id ? "bg-secondary text-secondary-foreground" : ""}`,
+                  )}
+                >
+                  <SheetClose
+                    onClick={() => {
+                      loadSongDetails({
+                        id: song.id!,
+                        songArtist: song.artist.name,
+                        songName: song.name!,
+                      });
+                    }}
+                  >
+                    <Image
+                      src={song.album ? song.album.artwork.url : noImage}
+                      alt={song.name || ""}
+                      width={song.album ? song.album.artwork?.width : 75}
+                      height={song.album ? song.album.artwork?.height : 75}
+                      className=" block h-auto w-full max-w-full rounded-md object-cover  sm:h-[75px] sm:w-[75px]"
+                      sizes={`75px`}
+                    />
+                  </SheetClose>
+                  <div className="flex flex-col items-start justify-start">
+                    <CardHeader className="line-clamp-1 p-0">
+                      <SheetClose
+                        onClick={() => {
+                          loadSongDetails({
+                            id: song.id!,
+                            songArtist: song.artist.name,
+                            songName: song.name!,
+                          });
+                        }}
+                      >
+                        {song.name}
+                      </SheetClose>
+                    </CardHeader>
+                    <CardDescription>
+                      <SheetClose
+                        onClick={() => {
+                          router.push(`/artist/${song.artist.id}`);
+                        }}
+                      >
+                        {song.artist.name}
+                      </SheetClose>
+                    </CardDescription>
+                  </div>
+                </Card>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
     </div>
   );
 };
--- src/components/hero/AlbumSongHero.tsx
+++ src/components/hero/AlbumSongHero.tsx
@@ -1,94 +1,59 @@
-import { Database } from "@/types/supabase";
-import React from "react";
+import { DeckState } from "@/stores/deck-store";
+import { Database, Tables } from "@/types/supabase";
+import { dateFormatter } from "@/utils/dateFormatter";
 import Image from "next/image";
 import Link from "next/link";
+import { ReactNode } from "react";
 import noImage from "../../../public/noimage.svg";
-import { Button } from "../ui/button";
-import { FaPlay } from "react-icons/fa6";
-import { dateFormatter } from "@/utils/dateFormatter";
-
-type Band = Omit<
-  Database["public"]["Tables"]["band"]["Row"],
-  "avatar_id" | "created_at"
->;
-
-type Artist = Omit<
-  Database["public"]["Tables"]["artist"]["Row"],
-  "avatar_id" | "band_id" | "created_at"
->;
-
-type Album = Omit<
-  Database["public"]["Tables"]["album"]["Row"],
-  "artist_id" | "artwork_id" | "band_id" | "created_at"
->;
 
 interface Props {
-  opts:
-    | {
-        dataType: "album";
-        artwork: {
-          url:
-            | Database["public"]["Tables"]["album"]["Row"]["artwork_id"]
-            | undefined;
-          title: Database["public"]["Tables"]["album"]["Row"]["title"];
-          width:
-            | Database["public"]["Tables"]["artwork"]["Row"]["width"]
-            | undefined;
-          height:
-            | Database["public"]["Tables"]["artwork"]["Row"]["height"]
-            | undefined;
-          description:
-            | Database["public"]["Tables"]["artwork"]["Row"]["description"]
-            | undefined;
-        };
-        name: Database["public"]["Tables"]["album"]["Row"]["title"];
-        band: Band | null;
-        artist: Artist | null;
-        release_date:
-          | Database["public"]["Tables"]["album"]["Row"]["release_date"]
-          | undefined;
-        songsLength: number;
-      }
-    | {
-        dataType: "song";
-        artwork: {
-          url:
-            | Database["public"]["Tables"]["album"]["Row"]["artwork_id"]
-            | undefined;
-          title: Database["public"]["Tables"]["album"]["Row"]["title"];
-          width:
-            | Database["public"]["Tables"]["artwork"]["Row"]["width"]
-            | undefined;
-          height:
-            | Database["public"]["Tables"]["artwork"]["Row"]["height"]
-            | undefined;
-          description:
-            | Database["public"]["Tables"]["artwork"]["Row"]["description"]
-            | undefined;
-        };
-        name: Database["public"]["Tables"]["album"]["Row"]["title"];
-        band: Band | null;
-        artist: Artist | null;
-        release_date:
-          | Database["public"]["Tables"]["album"]["Row"]["release_date"]
-          | undefined;
-        songsLength: number;
-        album: Album | null;
-      };
+  album: Tables<"album"> | null;
+  artist: Tables<"artist">;
+  artwork: Tables<"artwork"> | null | undefined;
+  dataType: "album" | "song";
+  name: Database["public"]["Tables"]["album"]["Row"]["title"];
+  playBtn: ReactNode;
+  playlist: DeckState["playlist"];
+  release_date:
+    | Database["public"]["Tables"]["album"]["Row"]["release_date"]
+    | undefined;
+  songsLength?: number;
+  trackId: Database["public"]["Tables"]["song"]["Row"]["id"];
 }
 
-const AlbumSongHero = ({ opts }: Props) => {
+const AlbumSongHero = ({
+  album,
+  artist,
+  artwork,
+  dataType,
+  name,
+  playBtn,
+  playlist,
+  release_date,
+  songsLength = 0,
+  trackId,
+}: Props) => {
+  const getSongDetails = () => {
+    return {
+      id: trackId,
+      songArtist: artist ? artist.name : "No Name",
+      songName: name,
+    };
+  };
+
+  const songDetails = getSongDetails();
+
   return (
     <div>
       {/* <div>
-        <pre>{JSON.stringify(data, null, 4)}</pre>
+        <pre>{JSON.stringify(playlist, null, 4)}</pre>
       </div> */}
       <div className="hero flex flex-col gap-4 sm:flex-row">
         <Image
-          src={opts.artwork.url || noImage}
-          alt={`${opts.name}${opts.artwork?.description && " - " + opts.artwork.description}`}
-          width={opts.artwork?.width}
-          height={opts.artwork?.height}
+          src={artwork?.url || noImage}
+          alt={`${name}${artwork?.description && " - " + artwork.description}`}
+          width={artwork?.width}
+          height={artwork?.height}
           className=" block h-auto w-full max-w-full rounded-md object-cover  sm:h-[300px] sm:w-[300px]"
           sizes={`
             (max-width: 375px) 310px,
@@ -98,55 +63,39 @@ const AlbumSongHero = ({ opts }: Props) => {
         />
         <div className="flex grow flex-col justify-end gap-4">
           <div>
-            <p>{opts.dataType}</p>
+            <p>{dataType}</p>
             <h1
               className="line-clamp-1 text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl"
-              title={opts.name}
+              title={dataType === "album" && album ? album?.title : name}
             >
-              {opts.name}
+              {dataType === "album" && album ? album?.title : name}
             </h1>
             <h2 className="text-md  md:text-lg">
               <strong>
-                {opts.band && (
+                {artist && (
                   <>
-                    <Link href={`/band/${opts.band.id}`}>{opts.band.name}</Link>{" "}
-                    &bull;
-                  </>
-                )}
-                {opts.artist && (
-                  <>
-                    <Link href={`/artist/${opts.artist.id}`}>
-                      {opts.artist.name}
-                    </Link>{" "}
+                    <Link href={`/artist/${artist.id}`}>{artist.name}</Link>{" "}
                     &bull;
                   </>
                 )}
               </strong>{" "}
-              {opts.dataType === "song" && (
+              {dataType === "song" && (
                 <>
-                  <Link href={`/album/${opts.album?.id}`}>
-                    {opts.album?.title}
-                  </Link>{" "}
+                  <Link href={`/album/${album?.id}`}>{album?.title}</Link>{" "}
                   &bull;
                 </>
               )}{" "}
-              {opts.release_date && (
-                <>{dateFormatter(opts.release_date)} &bull;</>
-              )}{" "}
-              {opts.dataType === "song" && <>02:38</>}
-              {opts.songsLength > 0 && opts.dataType === "album" && (
+              {release_date && <>{dateFormatter(release_date)} &bull;</>}{" "}
+              {dataType === "song" && <>02:38</>}
+              {songsLength > 0 && dataType === "album" && (
                 <>
-                  {opts.songsLength}{" "}
-                  {opts.songsLength === 1 ? <>track</> : <>track-uri</>}
+                  {songsLength}{" "}
+                  {songsLength === 1 ? <>track</> : <>track-uri</>}
                 </>
               )}
             </h2>
           </div>
-          <div className="">
-            <Button className="h-10 w-10 rounded-full p-1" variant={"default"}>
-              <FaPlay size={20} className="relative -right-[1px]" />
-            </Button>
-          </div>
+          <div className="">{playBtn}</div>
         </div>
       </div>
     </div>
